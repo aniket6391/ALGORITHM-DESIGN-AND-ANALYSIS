ae9ae1898bc2a6b9650f55101aa98060176fbf5a
aniket6391 <aniket6391915300@gmail.com>
2026-08-06 20:49:46 +0530

    Auto Update 2026-08-06 20:49:46

--- Array_reverse.cpp
+++ Array_reverse.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-int arr[]={1,2,3,4,5};
+int arr[]={1,2,3,4};
 int n=5;
 reverse(arr,arr+n);
 cout<<"Reverse array: ";
-for(int i=0;i<5;i++){
+for(int i=0;i<4;i++){
     cout<<arr[i]<<" ";
 }
 return 0;
