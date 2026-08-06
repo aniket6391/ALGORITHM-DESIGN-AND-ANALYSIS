acb902dc599f790968f601df451e2dcaea3f8bc3
aniket6391 <aniket6391915300@gmail.com>
2026-08-06 22:13:55 +0530

    Auto Update 2026-08-06 22:13:55

--- LCM.cpp
+++ LCM.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    cout<<"Enter numbers: ";
-    cin>>a>>b;
-    int lcm=(a>b)?a:b
-while(true){
+int a,b;
+cout<<"Enter numbers: ";
+cin>>a>>b;
+int lcm=(a>b)?a:b;
+while(true)
+{
     if(lcm%a==0 && lcm%b==0){
-    cout<<"LCM: "<<lcm;
-break;
-}
-lcm++;
+        cout<<"LCM: "<<lcm;
+        break;
+    }
+   lcm++;
 }
 return 0;
 }
