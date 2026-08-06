100169c74b93ebad291235b729ae4563af9af539
aniket6391 <aniket6391915300@gmail.com>
2026-08-06 22:25:11 +0530

    Auto Update 2026-08-06 22:25:11

---
+++ Power_of_n.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int base,exp;
+    cout<<"Enter base :";
+    cin>>base;
+
+    cout<<"Enter exponent: ";
+    cin>>exp; 
+    return 0;
+}
