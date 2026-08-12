c6bc48be2b08060eb5605ce56c4333cdff9aa417
aniket6391 <aniket6391915300@gmail.com>
2026-08-12 15:09:32 +0530

    Auto Update 2026-08-12 15:09:32

--- Binary_search_using_forloop.cpp
+++ Binary_search_using_forloop.cpp
@@ -1,25 +1,26 @@
 #include<iostream>
 using namespace std;
 int main(){
-int arr[]={10,20,30,40,50};
-int n=5;
+int arr[]={1,2,3,4,5,6,7};
+int n=7;
 int key;
 
-cout<<"Enter element to search: ";
-cin>>key;
-int low=0,  high=n-1;
-for(int i=0;low<=high;i++){
+ cout<<"Enter element to search: ";
+ cin>>key;
+
+ int low=0, high =n-1;
+ for(int i=0;low<=high;i++){
     int mid=(low+high)/2;
 
     if(arr[mid]==key){
-        cout<<"Element found at index: ";
-        return 0;
-    }
-    else if(arr[mid]<key)
-    low=mid-1;
-else
-high=mid-1;
+        cout<<"Element found at index: "<<mid;
+    return 0;
+ }
+ else if(arr[mid]<key)
+ low=mid+1;
+ else
+    high =mid-1;
 }
-cout<<"Element not found";
-return 0;
+    cout<<"Element not found";
+    return 0;
 }
