4b323f99064b5644286f42e9e590c4a1b7647a25
aniket6391 <aniket6391915300@gmail.com>
2026-08-18 20:33:38 +0530

    Auto Update 2026-08-18 20:33:38

--- linearSearch_usingFunction.cpp
+++ linearSearch_usingFunction.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
 using namespace std;
-int linearSerch(int arr[],int n,int key){
-    for(int i=0;i<n;i++){
+int linearSearch(int arr[],int n,int key){
+for(int i=0;i<n;i++){
     if(arr[i]==key){
         return i;
     }
 }
 return -1;
 }
+
 int main(){
-    int arr[5]={7,8,5,1,2};
-    int key=1;
-    int result=linerarSearch(arr,5,key);
-    if(result==-1)
+int arr[5]={5,3,6,2,1};
+int key=2;
+
+int result=linearSearch(arr,5,key);
+if(result==-1)
     cout<<"Element not found";
 else
-cout<<"Element found at index: "<<result;
+    cout<<"Element found at index: "<<result;
 return 0;
 }
