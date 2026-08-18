f38bfbe965cc9e5dc51ca60df018bdeb9c7cc952
aniket6391 <aniket6391915300@gmail.com>
2026-08-18 23:17:42 +0530

    Auto Update 2026-08-18 23:17:42

--- BinarySearch_usingFunction.cpp
+++ BinarySearch_usingFunction.cpp
@@ -7,28 +7,25 @@ int high=n-1;
 while(low<=high){
     int mid=(low+high)/2;
     if(arr[mid]==key){
-        cout<<"Element found at index: "<<mid;
+        return mid;
     }
     else if(arr[mid]<key)
-    low=mid+1;
-else
-high=mid-1;
+        low=mid+1;
+    else
+        high=mid-1;
 }
 return -1;
 }
 int main(){
-int arr[]={1,2,3,4,5,6,7,8,9,10};
+int arr[]{1,2,3,4,5,6,7,8,9,10};
 int key;
 cout<<"Enter search element: ";
 cin>>key;
-
-
 int pos=binarySearch(arr,10,key);
 if(pos==-1)
-cout<<"element not found";
-else
-cout<<"Element found at index: ";
-return 0;
-
+    cout<<"Element not found";
+    else
+        cout<<"Element found at index: "<<pos;
+    return 0;
 
 }
