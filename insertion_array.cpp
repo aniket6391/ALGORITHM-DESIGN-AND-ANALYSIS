c81b9072fb562bd5f10057414d86a82ded6ff7d8
aniket6391 <aniket6391915300@gmail.com>
2026-08-27 12:24:25 +0530

    Auto Update 2026-08-27 12:24:25

---
+++ insertion_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[100]={10,20,30,40,50};
+int n=5;
+int pos=3;
+int val=35;
+for(int i=0;i<n;i--){
+    arr[i]=arr[i-1];
+}
+arr[pos]=value;
+n++;
+}
+cout<<"After insertion: ";
+for(int 0;i<n;i++){
+    cout<<arr[i]<<" ";
+    return 0;
+}
