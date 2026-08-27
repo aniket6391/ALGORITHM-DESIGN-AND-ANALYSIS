efeeebeadfe2cc1cf5c2988b7ed8473f2860be37
aniket6391 <aniket6391915300@gmail.com>
2026-08-27 12:14:08 +0530

    Auto Update 2026-08-27 12:14:07

--- Searching _in_array.cpp
+++ Searching _in_array.cpp
@@ -1,17 +1,16 @@
-//linear search
 #include<iostream>
 using namespace std;
 int main(){
-int arr[]={10,20,30,40,50};
-int n=5;
-int key=40;
-for(int i=0;i<n;i++){
-    if(arr[i]==key)
-{
-    cout<<"Element found at index: "<<i;
-return 0;
-}
-}
-cout<<"Element not found";
+    int arr[]={1,2,3,4,5};
+    int n=5;
+    int key=3;
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            cout<<"Element found at index: ";<<i;
+            return 0;
+
+        }
+    }
+cout<<"Elemnt not found";
 return 0;
 }
